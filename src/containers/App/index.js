cf75928ec0b17b2f0f6af1515ec68d49439742f6
joonhyeok <joonhyeok.ahn3@wework.com>
2022-11-28 08:38:30 -0600

    Include the footer

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,7 @@
-import React  from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Nav from "../../components/Nav";
+import Footer from "../../components/Footer/";
 import HomeContainer from "../Home";
 import ArticlesContainer from "../Articles";
 import ArticleContainer from "../Article";
@@ -11,15 +12,16 @@ function App() {
   return (
     <div className="App">
       <Nav />
-        <Routes>
-          <Route path="/" element={<HomeContainer />} exact />
-          <Route path="/home" element={<HomeContainer />} exact />
-          <Route path="/articles" element={<ArticlesContainer />} exact />
-          <Route path="/article/:slug" element={<ArticleContainer />} exact />
-          <Route path="/category/:slug" element={<CategoriesContainer />} exact />
-          <Route path="/projects" element={<ProjectsContainer />} exact />
-        </Routes>
-        </div>
+      <Routes>
+        <Route path="/" element={<HomeContainer />} exact />
+        <Route path="/home" element={<HomeContainer />} exact />
+        <Route path="/articles" element={<ArticlesContainer />} exact />
+        <Route path="/article/:slug" element={<ArticleContainer />} exact />
+        <Route path="/category/:slug" element={<CategoriesContainer />} exact />
+        <Route path="/projects" element={<ProjectsContainer />} exact />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
