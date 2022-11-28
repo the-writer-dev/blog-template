dcba0864fca31e6b9787619c36a199a12fe94166
joonhyeok <joonhyeok.ahn3@wework.com>
2022-11-28 07:44:03 -0600

    Update the progressbar

--- src/components/Query/index.js
+++ src/components/Query/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
+import { Hourglass } from "react-spinners-css";
+
+const style = {
+  opacity: 0.5,
+}
 
 const Query = ({ children, query, slug }) => {
   const { data, loading, error } = useQuery(query, {
     variables: { slug: slug },
   });
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <Hourglass color="#6b6965" size="50" style={style}/>; 
   if (error) return <p>Error: {JSON.stringify(error)}</p>;
   return children({ data });
 };
