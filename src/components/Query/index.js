ad46e16b6abfa52381c10ee836b156ebcbe4d2dc
joonhyeok <joonhyeok.ahn3@wework.com>
2022-11-29 09:47:41 -0600

    Clean up the unused imports and components

--- src/components/Query/index.js
+++ src/components/Query/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
-import { Hourglass } from "react-spinners-css";
-
-const style = {
-  opacity: 0.5,
-}
+// import { Hourglass } from "react-spinners-css";
+//
+// const style = {
+//   opacity: 0.5,
+// };
 
 const Query = ({ children, query, slug }) => {
   const { data, loading, error } = useQuery(query, {
     variables: { slug: slug },
   });
 
-  if (loading) return <Hourglass color="#6b6965" size="50" style={style}/>; 
+  if (loading) return <p>Loading ...</p>;
+  // return <Hourglass color="#6b6965" size="50" style={style}/>;
   if (error) return <p>Error: {JSON.stringify(error)}</p>;
   return children({ data });
 };
--- src/containers/Articles/index.js
+++ src/containers/Articles/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import Articles from "../../components/Articles";
 import Categories from "../../components/Categories";
 import Query from "../../components/Query";
